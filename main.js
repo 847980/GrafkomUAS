d6f89d46a7352d404430359c17ea55c409086802
847980 <sean.christophe123@gmail.com>
2024-05-21 14:53:26 +0700

    tes

--- main.js
+++ main.js
@@ -3,51 +3,108 @@ import './style.css'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
+import { Water } from 'three/addons/objects/Water.js';
+import { Water as Water2 } from 'three/addons/objects/Water2.js';
+
+const clock = new THREE.Clock();
+
+let mixer;
+
+//OCEAN and river
+let ocean, water;
+const params = { //river
+  color: '#ffffff',
+  scale: 4,
+  flowX: 1,
+  flowY: 1
+};
+
 
 //SKY
 let sky, sun;
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 //setup Scene and Camera
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth
-  /window.innerHeight,0.1,1000);
-camera.position.set(0,0,100);
-camera.lookAt(0,0,0);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth
+  / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 3, 100);
+camera.lookAt(0, 0, 0);
 
 //Orbit Controls
-const controls = new OrbitControls(camera,renderer.domElement);
-controls.target.set(0,5,0);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 5, 0);
 controls.update();
 
 //LIGHT
 //Directional Light
 var color = 0xFFFFFF;
 var light = new THREE.DirectionalLight(color, 0.5);
-light.position.set(0,10,0);
-light.target.position.set(-5,0,0);
+light.position.set(0, 10, 0);
+light.target.position.set(-5, 0, 0);
 scene.add(light);
 scene.add(light.target);
 
 //Hemisphere Light (warna langit)
-                                  //skycolor //groundColor //intensity
+//skycolor //groundColor //intensity
 light = new THREE.HemisphereLight(0xB1E1FF, 0xB97A20, 5);
 scene.add(light);
 
 //Point Light (warna dari lampu)
-                            //color  //intensity
-light = new THREE.PointLight(0xFFFF00,50);
-light.position.set(10,10,0);
+//color  //intensity
+light = new THREE.PointLight(0xFFFF00, 50);
+light.position.set(10, 10, 0);
 scene.add(light);
 
 //Spot Light
-                            //color  //intensity
-light = new THREE.SpotLight(0xFF0000,50);
-light.position.set(10,10,0);
+//color  //intensity
+light = new THREE.SpotLight(0xFF0000, 50);
+light.position.set(10, 10, 0);
 scene.add(light);
+
+//OCEAN
+const waterGeometry = new THREE.PlaneGeometry(10000, 10000);
+
+ocean = new Water(
+  waterGeometry,
+  {
+    textureWidth: 512,
+    textureHeight: 512,
+    waterNormals: new THREE.TextureLoader().load('textures/waternormals.jpg', function (texture) {
+
+      texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+
+    }),
+    sunDirection: new THREE.Vector3(),
+    sunColor: 0xffffff,
+    waterColor: 0x001e0f,
+    distortionScale: 3.7,
+    fog: scene.fog !== undefined
+  }
+);
+
+ocean.rotation.x = - Math.PI / 2;
+
+scene.add(ocean);
+
+// river
+const riverGeometry = new THREE.PlaneGeometry(48, 6.5);
+
+water = new Water2(riverGeometry, {
+  color: params.color,
+  scale: params.scale,
+  flowDirection: new THREE.Vector2(params.flowX, params.flowY),
+  textureWidth: 1024,
+  textureHeight: 1024
+});
+
+water.position.set(-12,2,20);
+water.rotation.x = Math.PI * - 0.5;
+scene.add(water);
 
 //Sky
 sky = new Sky();
@@ -68,53 +125,64 @@ const theta = THREE.MathUtils.degToRad(180); //azimuth
 sun.setFromSphericalCoords(1, phi, theta);
 
 uniforms['sunPosition'].value.copy(sun);
+ocean.material.uniforms['sunDirection'].value.copy(sun).normalize();
 
 renderer.toneMappingExposure = 0.2;
 
-
-//Geometry
-const objects = [];
-
-//plane
-{
-  var planetGeo = new THREE.PlaneGeometry(200,200);
-  var planetMat = new THREE.MeshPhongMaterial({color: '#8AC'});
-  const mesh = new THREE.Mesh(planetGeo, planetMat);
-  mesh.rotation.x = Math.PI * -0.5;
-  scene.add(mesh);
-}
-
 // instantiate a loader
-const onProgress = function ( xhr ) {
+const onProgress = function (xhr) {
 
-  if ( xhr.lengthComputable ) {
+  if (xhr.lengthComputable) {
 
     const percentComplete = xhr.loaded / xhr.total * 100;
-    console.log( percentComplete.toFixed( 2 ) + '% downloaded' );
+    console.log(percentComplete.toFixed(2) + '% downloaded');
 
   }
 
 };
 
-const loader = new GLTFLoader().setPath( 'resources/day/' );
-	loader.load( 'map_village_day.gltf', async function ( gltf ) {
+const loader = new GLTFLoader().setPath('resources/env/');
+loader.load('envi.gltf', async function (gltf) {
 
-		const model = gltf.scene;
-    model.position.set(0,-6.5,0);
-		scene.add( model );
-			
-	} );
+  const model = gltf.scene;
+  model.position.set(0, -5, 0);
+  scene.add(model);
+
+});
+
+// const loader2 = new FBXLoader();
+// loader2.load( 'resources/wolf1/Wolf_IP_Attack_Bite_Front_PreviewMesh.fbx', function ( object ) {
+
+//   mixer = new THREE.AnimationMixer( object );
+
+//   const action = mixer.clipAction( object.animations[ 0 ] );
+//   action.play();
+
+//   object.traverse( function ( child ) {
+
+//     if ( child.isMesh ) {
+
+//       child.castShadow = true;
+//       child.receiveShadow = true;
+
+//     }
+
+//   } );
+
+//   scene.add( object );
+
+// } );
 
 var time_prev = 0;
-function animate(time){
+function animate(time) {
   var dt = time - time_prev;
   dt *= 0.1;
 
-  objects.forEach((obj)=>{
-    obj.rotation.z += dt * 0.01;
-  });
+  // const delta = clock.getDelta();
 
-  renderer.render(scene,camera);
+  // if ( mixer ) mixer.update( delta );
+  ocean.material.uniforms['time'].value += 1.0 / 60.0;
+  renderer.render(scene, camera);
 
   time_prev = time;
   requestAnimationFrame(animate);
