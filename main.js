a2ef99ec3e8c28f02baf74db86a72ddea697c780
847980 <137378009+847980@users.noreply.github.com>
2024-07-03 21:08:43 +0700

    resert fpp

--- main.js
+++ main.js
@@ -123,6 +123,11 @@ gui.add(guiElements, "cameras", ['Free', 'TPP', 'FPP']).name("Camera").onChange(
     player.controller = new PlayerController();
     orbital.enable(orbital._disabled);
   } else if (value == 'TPP') {
+    if (player.camera instanceof FirstPersonCamera) {
+      console.log("true");
+      player.camera.setup((player.mesh.position.x, player.mesh.position.y + 1.6, player.mesh.position.z - 0.3),new THREE.Vector3(0, 0, 0));
+      console.log(player.camera.camera.rotation);
+    }
     tpp = true;
     fpp = false;
     controls.autoRotate = false;
